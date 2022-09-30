b5629964721c3de78cb6e892d52af07f6ee3446f
Vincent Nguyen <vincent.nguyen@anu.edu.au>
2022-10-01 00:32:17 +1000

    Bumped and added analysis-tools version

--- setup.py
+++ setup.py
@@ -9,5 +9,12 @@ setup(
     license='GNU GPLv3',
     author='Vincent Nguyen',
     author_email='vincent.nguyen@anu.edu.au',
-    description='Dense Bi-encoders for end-to-end Dense Information Retrieval'
+    description='Dense Bi-encoders for end-to-end Dense Information Retrieval',
+    install_requires=['torch', 'elasticsearch[async]==8.3.1', 'plac', 'sentence-transformers==2.2.2', 'spacy~=3.2.4',
+                      'scispacy', 'https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz',
+                      'tqdm~=4.64.0', 'pandas', 'trectools', 'numpy~=1.22.4', 'toml~=0.10.2',
+                       'https://github.com/Ayuei/analysis_tools/releases/download/python/analysis_tools_ir-0.0.1-py3-none-any.whl',
+                      'dill~=0.3.5.1', 'requests~=2.28.0', 'loguru~=0.6.0', 'git+https://github.com/Ayuei/allRank.git',
+                      'transformers==4.22.0', 'shutup~=0.2.0', 'datasets==2.4.0', 'jupyterlab==3.4.7',
+                      'scikit-learn==1.1.2', 'scipy~=1.9.1', 'optuna==3.0.2', 'wandb==0.13.3', 'torch_optimizer']
 )
