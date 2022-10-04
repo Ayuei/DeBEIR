e9a34964f52e9751e87b047d4ff0c8bc0a8b36b5
Vincent Nguyen <vincent.nguyen@anu.edu.au>
2022-10-04 23:12:34 +1100

    Added dependency link

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Dense Bi-encoders for end-to-end Dense Information Retrieval',
     install_requires=['torch', 'elasticsearch[async]==8.3.1', 'plac', 'sentence-transformers==2.2.2', 'spacy~=3.2.4',
                       'scispacy',
-                      "en_core_sci_md @ git+https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz",
+                      "en_core_sci_md",
                       "analysis_tools_ir",
                       'tqdm~=4.64.0', 'pandas', 'trectools', 'numpy~=1.22.4', 'toml~=0.10.2',
                       'dill~=0.3.5.1', 'requests~=2.28.0', 'loguru~=0.6.0',
@@ -20,6 +20,7 @@ setup(
                       'transformers==4.22.0', 'shutup~=0.2.0', 'datasets==2.4.0', 'jupyterlab==3.4.7',
                       'scikit-learn==1.1.2', 'scipy~=1.9.1', 'optuna==3.0.2', 'wandb==0.13.3', 'torch_optimizer'],
     dependency_link = [
-        "https://github.com/Ayuei/analysis_tools_ir/releases/download/v0.0.3/analysis_tools_ir-0.0.3-py3-none-any.whl"
+        "https://github.com/Ayuei/analysis_tools_ir/releases/download/v0.0.3/analysis_tools_ir-0.0.3-py3-none-any.whl",
+        "https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz"
     ]
 )
