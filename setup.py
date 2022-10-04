decd0f1fd4f7934fd6df8048ed72bfb1ffd8d0fd
Vincent Nguyen <vincent.nguyen@anu.edu.au>
2022-10-04 23:21:46 +1100

    Correct dependency for scispacy model

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Dense Bi-encoders for end-to-end Dense Information Retrieval',
     install_requires=['torch', 'elasticsearch[async]==8.3.1', 'plac', 'sentence-transformers==2.2.2', 'spacy~=3.2.4',
                       'scispacy',
-                      "en_core_sci_md",
+                      "en_core_sci_md @ https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz"
                       "analysis_tools_ir",
                       'tqdm~=4.64.0', 'pandas', 'trectools', 'numpy~=1.22.4', 'toml~=0.10.2',
                       'dill~=0.3.5.1', 'requests~=2.28.0', 'loguru~=0.6.0',
@@ -21,6 +21,5 @@ setup(
                       'scikit-learn==1.1.2', 'scipy~=1.9.1', 'optuna==3.0.2', 'wandb==0.13.3', 'torch_optimizer'],
     dependency_link = [
         "https://github.com/Ayuei/analysis_tools_ir/releases/download/v0.0.3/analysis_tools_ir-0.0.3-py3-none-any.whl",
-        "https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz"
     ]
 )
