476161e2e049069188b386a11c81d9ef45ae678a
Ayuei <sefton_student@hotmail.com>
2022-10-17 16:07:12 +1100

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from nir import *
-import os
-
-__all__ = ["data_sets", "engines", "evaluation", "interfaces", "models", "rankers", "training"]
-
-GLOBAL_PATH = os.path.abspath(os.path.dirname(__file__))
