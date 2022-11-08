f5ddd6894adf8c8ab8215879d3412b535026ebc8
Ayuei <sefton_student@hotmail.com>
2022-11-08 19:06:45 +1100

    Added Results object for storing ranked results

--- src/debeir/interfaces/results.py
+++ src/debeir/interfaces/results.py
@@ -1,61 +1,75 @@
-from typing import Dict, List, Union
+from typing import List
 from debeir.interfaces.document import Document
 from debeir.interfaces.document import document_factory
 
 
 LAZY_STATIC_DOCUMENT_KEY = "document_objects"
+LAZY_STATIC_DOCUMENT_TOPICS = "document_topics"
+LAZY_STATIC_DOCUMENT_HASHMAP = "document_topics"
 
 
 class Results:
     document_cls: Document
 
-    def __init__(self, results: List, engine_name):
+    def __init__(self, results: List, query_cls, engine_name):
         self.results = results
         self.document_cls: Document = document_factory[engine_name]
         self.__doc_cur = 0
         self.__topic_num = None
-        self.__topic_nums = []
         self.lazy_static = {}
+        self.query_cls = query_cls
+        self.topic_flag = False
 
-    def as_documents(self, recompile=False):
+    def _as_documents(self, recompile=False):
         if recompile or 'document_objects' not in self.lazy_static:
-            self.lazy_static[LAZY_STATIC_DOCUMENT_KEY] = self.document_cls.from_results(self.results)
+            self.lazy_static[LAZY_STATIC_DOCUMENT_KEY] = self.document_cls.from_results(self.results,
+                                                                                        self.query_cls,
+                                                                                        ignore_facets=False)
+            self.lazy_static[LAZY_STATIC_DOCUMENT_TOPICS] = list(self.lazy_static[LAZY_STATIC_DOCUMENT_KEY].keys())
 
         return self.lazy_static[LAZY_STATIC_DOCUMENT_KEY]
 
+    def get_topic_ids(self):
+        if LAZY_STATIC_DOCUMENT_KEY not in self.lazy_static:
+            self._as_documents()
+
+        return self.lazy_static[LAZY_STATIC_DOCUMENT_TOPICS]
+
     def __iter__(self):
+        self._as_documents()
         self.__doc_cur = 0
 
-        if self.__topic_num is None:
+        if not self.__topic_num:
             self.__topic_num = 0
 
         return self
 
     def __next__(self):
-        if self.__topic_nums:
-            topic_num = self.__topic_nums[self.__topic_num]
-        else:
+        if self.topic_flag:
             topic_num = self.__topic_num
+        else:
+            topic_num = self.get_topic_ids()[self.__topic_num]
 
-        if self.__doc_cur >= len(self.results[topic_num]):
-            if self.__topic_nums and topic_num > len(self.__topic_nums):
-                raise StopIteration
-
+        if self.__doc_cur >= len(self._as_documents()[topic_num]):
             self.__doc_cur = 0
             self.__topic_num += 1
 
-        item = self.results[topic_num][self.__doc_cur]
+            if self.topic_flag or self.__topic_num >= len(self.get_topic_ids()):
+                raise StopIteration
+
+            topic_num = self.get_topic_ids()[self.__topic_num]
+
+        item = self._as_documents()[topic_num][self.__doc_cur]
         self.__doc_cur += 1
 
         return item
 
     def __call__(self, topic_num=None):
         self.__topic_num = topic_num
+        if topic_num:
+            self.topic_flag = True
 
         return self
 
-    def get_topic_nums(self):
-        return self.results
-
     def __getitem__(self, item):
-        return self.results[item]
+        return self._as_documents()[item]
---
+++ tests/test_results.py
@@ -0,0 +1,200 @@
+import os
+import sys
+
+import pytest
+import toml
+
+from src.debeir.engines.client import Client
+from src.debeir.data_sets.factory import query_factory, parser_factory, executor_factory, config_factory
+from src.debeir.interfaces import config
+from src.debeir.interfaces.config import _NIRMasterConfig
+from src.debeir.interfaces.results import Results
+
+
+@pytest.fixture(scope="session")
+def config_file_dict(tmp_path_factory):
+    sys.path.insert(0, os.path.dirname("../main.py"))
+
+    config = {
+        "query_type": "embedding",
+        "automatic": True,
+        "encoder_fp": "sentence-transformers/distilbert-base-nli-mean-tokens",
+        "index": "test",
+        "norm_weight": 10,
+
+        "config_fn": "generic",
+        "query_fn": "generic",
+        "parser_fn": "tsv",
+        "executor_fn": "generic",
+
+        "qrels": "test_set/qrels_dev.tsv",
+        "topics_path": "test_set/queries.tsv",
+    }
+
+    fn = tmp_path_factory.mktemp("test_config") / "temp_config.toml"
+    toml.dump(config, open(fn, "w+"))
+
+    return fn, config
+
+
+@pytest.fixture(scope="session")
+def nir_config_dict(tmp_path_factory):
+    nir_str = """
+        [metrics]
+            [metrics.common]
+            metrics=["ndcg@10", "ndcg@20", "ndcg@100",
+                     "rprec@1000",
+                     "p@1", "p@5", "p@10", "p@15", "p@20",
+                     "bpref@1000",
+                     "recall@1000",
+                     "rprec@1000",
+                     "r@1000"]
+        
+        [search.engines]
+            [search.engines.elasticsearch]
+            protocol = "http"
+            ip = "localhost"
+            port = "9200"
+            timeout = 600
+        
+            [search.engines.solr]
+            ip = "127.0.0.1"
+            port = "9200"
+        
+        
+        [nir]
+            [nir.default_settings]
+            norm_weight = "2.15"
+            evaluate = false
+            return_size = 5 
+            output_directory = "./output/test/"
+    """
+
+    fn = tmp_path_factory.mktemp("test_config") / "temp_nir_config.toml"
+
+    nir_toml = toml.loads(nir_str)
+    toml.dump(nir_toml, open(fn, 'w+t'))
+
+    return fn, nir_toml
+
+
+@pytest.mark.asyncio
+async def test_iteration_topic_num(config_file_dict, nir_config_dict):
+    c = config.GenericConfig.from_toml(config_file_dict[0])
+
+    parser_cls = parser_factory[c.parser_fn]
+    topics = parser_cls._get_topics(c.topics_path)
+    query_cls = query_factory[c.query_fn]
+    engine_cls = executor_factory[c.executor_fn]
+
+    master_config = config_factory(nir_config_dict[0], _NIRMasterConfig)
+    engine_config = master_config.get_search_engine_settings(return_as_instance=True)
+    nir_settings = master_config.get_nir_settings(return_as_instance=True)
+
+    client = Client.build_from_config("elasticsearch", engine_config)
+
+    query_obj = query_cls(topics=topics, config=c)
+    engine = engine_cls.build_from_config(topics, query_obj, client.es_client, c,
+                                          nir_settings)
+
+    results = await engine.run_all_queries(return_results=True, cosine_offset=100, return_size=5)
+
+    r = Results(results, query_obj, "elasticsearch")
+
+    for (topic_num, res) in results:
+        r_itr_topic = iter(r(topic_num=topic_num))
+        for rank, result in enumerate(res["hits"]["hits"], start=1):
+            doc_id = query_cls.get_id_mapping(result["_source"])
+            doc = next(r_itr_topic)
+
+            assert doc.doc_id == doc_id
+            assert doc.topic_num == topic_num
+            assert doc.scores['rank'] == rank
+            assert doc.facets['Text'] == result['_source']['Text']
+
+@pytest.mark.asyncio
+async def test_iteration_no_topic_num(config_file_dict, nir_config_dict):
+    c = config.GenericConfig.from_toml(config_file_dict[0])
+
+    parser_cls = parser_factory[c.parser_fn]
+    topics = parser_cls._get_topics(c.topics_path)
+    query_cls = query_factory[c.query_fn]
+    engine_cls = executor_factory[c.executor_fn]
+
+    master_config = config_factory(nir_config_dict[0], _NIRMasterConfig)
+    engine_config = master_config.get_search_engine_settings(return_as_instance=True)
+    nir_settings = master_config.get_nir_settings(return_as_instance=True)
+
+    client = Client.build_from_config("elasticsearch", engine_config)
+
+    query_obj = query_cls(topics=topics, config=c)
+    engine = engine_cls.build_from_config(topics, query_obj, client.es_client, c,
+                                          nir_settings)
+
+    results = await engine.run_all_queries(return_results=True, cosine_offset=100, return_size=5)
+
+
+    r = Results(results, query_obj, "elasticsearch")
+
+    temp = {}
+
+    for topic_num, res in results:
+        temp[topic_num] = res
+
+    results = temp
+    r_itr_topic = iter(r)
+
+    for topic_num in r.get_topic_ids():
+        res = results[topic_num]
+
+        for rank, result in enumerate(res["hits"]["hits"], start=1):
+            doc_id = query_cls.get_id_mapping(result["_source"])
+            doc = next(r_itr_topic)
+
+            assert doc.doc_id == doc_id
+            assert doc.topic_num == topic_num
+            assert doc.scores['rank'] == rank
+            assert doc.facets['Text'] == result['_source']['Text']
+
+
+@pytest.mark.asyncio
+async def test_iteration_dict(config_file_dict, nir_config_dict):
+    c = config.GenericConfig.from_toml(config_file_dict[0])
+
+    parser_cls = parser_factory[c.parser_fn]
+    topics = parser_cls._get_topics(c.topics_path)
+    query_cls = query_factory[c.query_fn]
+    engine_cls = executor_factory[c.executor_fn]
+
+    master_config = config_factory(nir_config_dict[0], _NIRMasterConfig)
+    engine_config = master_config.get_search_engine_settings(return_as_instance=True)
+    nir_settings = master_config.get_nir_settings(return_as_instance=True)
+
+    client = Client.build_from_config("elasticsearch", engine_config)
+
+    query_obj = query_cls(topics=topics, config=c)
+    engine = engine_cls.build_from_config(topics, query_obj, client.es_client, c,
+                                          nir_settings)
+
+    results = await engine.run_all_queries(return_results=True, cosine_offset=100, return_size=5)
+    r = Results(results, query_obj, "elasticsearch")
+
+    temp = {}
+
+    for topic_num, res in results:
+        temp[topic_num] = res
+
+    results = temp
+
+    for topic_num in r.get_topic_ids():
+        res = results[topic_num]
+        result_set = r[topic_num]
+
+        for rank, result in enumerate(res["hits"]["hits"]):
+            doc_id = query_cls.get_id_mapping(result["_source"])
+            doc = result_set[rank]
+
+            assert doc.doc_id == doc_id
+            assert doc.topic_num == topic_num
+            assert doc.scores['rank'] == rank + 1
+            assert doc.facets['Text'] == result['_source']['Text']
