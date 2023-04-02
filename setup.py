a23fbe7a5a02035796bf560ed1bc2092aa303e47
Ayuei <sefton_student@hotmail.com>
2023-04-02 19:19:34 +1000

    Increment version number due to package changes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='DeBeIR',
-    version='0.1.0',
+    version='0.1.1',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     url='',
